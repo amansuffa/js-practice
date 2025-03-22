15dddd3641695b75a1fed7e33ad4ae0dc8801224
amansuffa <amansuffah@gmail.com>
2025-03-22 20:07:09 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hamza")
+
+
+let name = "meeru"
+let age = 7
+let isLoggedIn = false
+let state;
+
+// number =>limit 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => when you didn't assign a value
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
