b06e3ac8be68b6010f3f318513dcd76ac55b5602
amansuffa <amansuffah@gmail.com>
2025-03-27 02:47:00 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,38 @@
+const firstName = "hamza";
+const repoCount = 50;
+ 
+// console.log(firstName + repoCount + "Value");
+
+
+const gameName = new String('ameerhamza')
+
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('m'))
+
+
+const newString = gameName.substring(0,4) // minus me value nhi de skty
+// console.log(newString)
+
+const anotherString = gameName.slice(-10,6) // minus me value de skty => hain ulta start kre ga
+console.log(anotherString)
+
+const newStringOne = "   hamza    "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+const url = 'https://hamza.com/ameer%20hamza'
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('hamza'))
+// console.log(url.includes('meeru'))
+
+
+const gameNameNew='Muhammad-Ameer-Hamza'
+// console.log(gameNameNew.split('-'))
