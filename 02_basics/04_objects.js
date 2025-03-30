3f4364999a1d98a2fa5f0457ba7fcbb7a3a37c2d
amansuffa <amansuffah@gmail.com>
2025-03-30 13:24:06 +0000

    Object de-structure & JSON

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -28,7 +28,7 @@ const obj4 = {5: "a", 6: "b"}
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
 
 const obj3 = {...obj1, ...obj2, ...obj4} // spread operator
-console.log(obj3);
+// console.log(obj3);
 
 
 const users = [
@@ -46,12 +46,55 @@ const users = [
     },
 ]
 
-console.log(users[1].email);
+// console.log(users[1].email);
 
 // console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+// ##################### Object de-structure & JSON ################################
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// console.log(course.courseInstructor);
+
+
+const {courseInstructor} = course
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+
+
+// In React ##########
+// const navbar = ({company})=>{
+
+// }
+ 
+// navbar(company="hamza")
+
+
+// JSON #################
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
