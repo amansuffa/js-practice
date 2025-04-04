b3e43d3c87148e406c0ba80fdc4b37137250c9ef
amansuffa <amansuffah@gmail.com>
2025-04-04 11:23:50 +0000

    scope p-2 and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -18,3 +18,53 @@ console.log(c);
 
 
 // global scope , browser me alag h or codespace me node k through run krne se alag ho ga
+
+// ########################
+
+
+// closure in js --> child function can access parent function variables even after parent function has executed
+function one(){
+    const username = "hamza"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+
+if (true) {
+    const username = "hamza"
+    if (username === "hamza") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+// function hoisting
+console.log(addone(5)) // is me error nhi aay ga q k syntax different h
+
+function addone(num){
+    return num + 1
+}
+
+// declaration se pehle use nhi kr skte
+
+addTwo(5) // is trh se error aay ga
+// function expression
+const addTwo = function(num){
+    return num + 2
+}
