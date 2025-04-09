d1db67c6ab289103912e67a91105ffe630666378
amansuffa <amansuffah@gmail.com>
2025-04-09 19:05:58 +0000

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,51 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break; // break agr use nahi kiya to next case bhi chalega except default
+//     case 4:
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+
+
+const month = "march"
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
