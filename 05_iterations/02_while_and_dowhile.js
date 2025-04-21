acc7c349362eb6a4dd8dce902c0c5400410756f8
amansuffa <amansuffah@gmail.com>
2025-04-21 19:28:22 +0000

    do while

--- 05_iterations/02_while_and_dowhile.js
+++ 05_iterations/02_while_and_dowhile.js
@@ -11,3 +11,11 @@ while (arr < myArray.length) {
     //console.log(`Value is ${myArray[arr]}`);
     arr = arr + 1
 }
+
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
